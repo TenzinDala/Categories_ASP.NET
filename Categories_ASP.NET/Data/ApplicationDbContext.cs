b0fbdbae47be2f28fdb493be45527923b2330dcd
TenzinDala <tenzindala2000@gmail.com>
2023-03-17 23:59:29 -0400

    Added DataBase

---
+++ Categories_ASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Categories_ASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Categories_ASP.NET.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
