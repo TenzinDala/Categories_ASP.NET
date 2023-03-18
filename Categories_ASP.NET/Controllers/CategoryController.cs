1c73bb100286be6abd55c494175ec7c0379a5fbe
TenzinDala <tenzindala2000@gmail.com>
2023-03-18 14:14:29 -0400

    Added Bootsrap

---
+++ Categories_ASP.NET/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Categories_ASP.NET.Data;
+using Categories_ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Categories_ASP.NET.Controllers
+{   
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.categories;
+            return View(objCategoryList);
+        }
+    }
+}
